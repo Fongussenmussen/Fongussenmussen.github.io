c19fdfa0c8a599d0851a61096afee0f3c8018465
Andrii <zoops0mania@gmail.com>
2022-02-20 13:38:38 +0200

    for students

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
-let x = 'Я ебу коней';
-document.write(x);
+"use strict";
+
+// let age = prompt('Crjkmrj nt,t ktn','');
+// alert(`Тебе ${age} лет`);
+// let question = 'Ты настоящий пидарас!'
+// let result;
+// result = confirm(question);
+// alert(result);
+// let value = true;
+
+// alert(typeof(value));
