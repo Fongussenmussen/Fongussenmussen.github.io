5b85fef44928511b8b2eb534e6d2ed0e48506a0c
Andrii <zoops0mania@gmail.com>
2022-02-20 13:51:56 +0200

    rename js

--- js/script.js
+++ js/script.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
 "use strict";
+alert('Error');
 
-// let age = prompt('Crjkmrj nt,t ktn','');
-// alert(`Тебе ${age} лет`);
-// let question = 'Ты настоящий пидарас!'
-// let result;
-// result = confirm(question);
-// alert(result);
-// let value = true;
-
-// alert(typeof(value));
-=======
-
->>>>>>> 8229d3c088a193521487e95376eba80a13bd6ca7
